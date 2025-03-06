f6b62e879ec22f439c9380280990eea673af02b4
silveirasarah585 <silveirasarah585@gmail.com>
2025-03-06 02:21:58 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,13 @@
+
+int main() {
+  int x = 5;
+  int y = 10;
+  if (x > y) {
+    printf("X is greater than Y");
+  } else if (y > x) {
+    printf("Y is greater than X");
+  } else {
+    printf("X and Y are equal");
+  }
+  return 0;
+}
