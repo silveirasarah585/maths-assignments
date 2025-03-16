533446bd0e1376bc89be73e0684b63b01e1f8a3c
silveirasarah585 <silveirasarah585@gmail.com>
2025-03-16 02:50:51 +0000

    Add 10.c

---
+++ src/10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x = 5;
+    int y = 10;
+    printf("The value of x is %d\n", x);
+    printf("The value of y is %d\n", y);
+    return 0;
+}
