56a51c43309e50be38b44d90659d240098e3f449
silveirasarah585 <silveirasarah585@gmail.com>
2025-03-19 04:21:15 +0000

    Add 13.c

---
+++ src/13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int x = 5;
+    if (x > 2) {
+        printf("x is greater than 2");
+    } else {
+        printf("x is less than or equal to 2");
+    }
+    return 0;
+}
