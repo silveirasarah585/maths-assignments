8a89ff0f044a55325d34ac62a6ba71133fb91b29
silveirasarah585 <silveirasarah585@gmail.com>
2025-04-03 13:08:12 +0000

    Add 25.c

---
+++ src/25.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    if (num1 > num2) {
+        printf("%d is greater than %d\n", num1, num2);
+    } else if (num2 > num1) {
+        printf("%d is greater than %d\n", num2, num1);
+    } else {
+        printf("Both numbers are equal: %d = %d\n", num1, num2);
+    }
+
+    return 0;
+}
