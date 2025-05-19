d3ff9d7fa6027afec6ded17affda71fb35c60987
silveirasarah585 <silveirasarah585@gmail.com>
2025-05-19 05:04:11 +0000

    Add 38.c

---
+++ src/38.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    if (num1 > num2) {
+        printf("Num 1 is greater than Num 2\n");
+    } else {
+        printf("Num 1 is not greater than Num 2\n");
+    }
+
+    return 0;
+}
